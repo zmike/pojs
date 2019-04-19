6a10f471e1c6a072da0fc893324b6f54a86c9bec
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2019-04-19 18:24:47 -0400

    tab

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,69 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-const NavBar = () => {
-    return(
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { Paper } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import { Link, Route, BrowserRouter, Switch } from "react-router-dom";
+
+class NavBar extends React.Component {
+  state = {
+    value: 0
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
+
+  render() {
+    return (
+      <BrowserRouter>
         <div>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="title" color="inherit">
-                Path of JAVASCRIPT
-                </Typography>
-                <Typography variant="h1" color="inherit">
-                Tree
-                </Typography>
-            </Toolbar>
-        </AppBar>
+          <AppBar position="static">
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              indicatorColor="primary"
+              textColor="inherit"
+            >
+              <Tab label="Tree" component={Link} to="/tree" />
+              <Tab label="Skills" component={Link} to="/skills" />
+            </Tabs>
+          </AppBar>
+          <Switch>
+            <Route path="/tree" component={PageShell(Tree)} />
+            <Route path="/skills" component={PageShell(Skills)} />
+          </Switch>
         </div>
-    )
+      </BrowserRouter>
+    );
+  }
 }
+function Tree(theme) {
+  return (
+    <Paper>
+      <div>Treeeeee</div>
+    </Paper>
+  );
+}
+
+function Skills(theme) {
+  return (
+    <Paper>
+      <div>Skillllsssss</div>
+    </Paper>
+  );
+}
+
+const PageShell = (Page, previous) => {
+  return props => (
+    <div className="page">
+      <Page {...props} />
+    </div>
+  );
+};
 export default NavBar;
